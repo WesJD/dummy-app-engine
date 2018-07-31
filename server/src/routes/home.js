967ec15dbfe42ab791a8b62065fd2f5bc561c996
Wesley Smith <me@wesjd.net>
2018-07-31 09:18:27 -0400

    start projects page

--- server/src/routes/home.js
+++ server/src/routes/home.js
@@ -3,7 +3,26 @@ import { getAuthorizedRouter } from "../utils/routerutil"
 const router = getAuthorizedRouter()
 
 router.get("/", (req, res) => {
-    res.render("home")
+    res.render("home", {
+        projects: [
+            {
+                name: "My Project",
+                description: "This project is really great and I am making it for class."
+            },
+            {
+                name: "My Project",
+                description: "This project is really great and I am making it for class. hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello "
+            },
+            {
+                name: "My Project",
+                description: "This project is really great"
+            },
+            {
+                name: "My Project",
+                description: "This "
+            },
+        ]
+    })
 })
 
 export default router 
