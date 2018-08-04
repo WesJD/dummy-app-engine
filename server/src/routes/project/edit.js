1447c6c845bff30d4531ea0018141ed2e06c9a05
Wesley Smith <me@wesjd.net>
2018-08-04 12:43:46 -0400

    prevent people from viewing projects that aren't theirs

--- server/src/routes/project/edit.js
+++ server/src/routes/project/edit.js
@@ -3,7 +3,10 @@ import { getAuthorizedRouter } from "../../utils/routerutil"
 const router = getAuthorizedRouter()
 
 router.get("/", (req, res) => {
-    res.render("edit")
+    const hash = req.query.hash
+    if (req.session.projects.some(project => project.hash == hash)) {
+        res.render("edit")
+    } else res.redirect("/projects")
 })
 
 export default router
